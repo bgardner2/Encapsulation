dca1e5e19e84355520ea954f9d9061790e6c5ef1
Ben <Ben@Gardner-PC>
2013-09-08 11:50:21 -0500

    Lab 3 Complete. Encapsulated necessary Methods and implemented them properly. Sort Function not working properly (not sure if it ever was)

--- src/lab3/PartsManager.java
+++ src/lab3/PartsManager.java
@@ -1,10 +1,11 @@
 package lab3;
 
 public class PartsManager {
+    private final int MAX_RECS = 10;
 
-    private String[] partNums = new String[10];
-    private String[] partDescs = new String[10];
-    private double[] partPrices = new double[10];
+    private String[] partNums = new String[MAX_RECS];
+    private String[] partDescs = new String[MAX_RECS];
+    private double[] partPrices = new double[MAX_RECS];
     private int emptyRow;
 
     public void addPart(String partNum, String partDesc, double partPrice) {
@@ -18,6 +19,19 @@ public class PartsManager {
         setPartDesc(partDesc, index);
         setPartPrice(partPrice, index);
     }
+    
+    
+    public int getPartNumLength(){
+        return partNums.length;
+    }
+    
+    public int getPartDescLength(){
+        return partDescs.length;
+    }
+    
+    public int getPartPricesLength(){
+        return partPrices.length;
+    }
 
     public String getPartNumAtIndex(int index) {
         return partNums[index];
@@ -29,6 +43,30 @@ public class PartsManager {
 
     public double getPriceAtIndex(int index) {
         return partPrices[index];
+    }
+    
+    public void SortAll(){
+        // Only perform the sort if we have records
+        if (emptyRow > 0) {
+            // Bubble sort routine adapted from sample in text book...
+            // Make sure the operations are peformed on all 3 arrays!
+            for (int passNum = 1; passNum < emptyRow + 1; passNum++) {
+                for (int i = 1; i <= emptyRow - passNum; i++) {
+                    String temp = "";
+                    temp += partPrices[i - 1];
+                    partPrices[i - 1] = partPrices[i];
+                    partPrices[i] = Double.parseDouble(temp);
+
+                    temp = partNums[i - 1];
+                    partNums[i - 1] = partNums[i];
+                    partNums[i] = temp;
+
+                    temp = partDescs[i - 1];
+                    partDescs[i - 1] = partDescs[i];
+                    partDescs[i] = temp;
+                }
+            }
+        }
     }
 
     private void setPartNum(String partNum, int index) {
